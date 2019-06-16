6848326657da23ba43d30b19d6a25540e427b7c8
sileixinhua <sileixinhua@163.com>
2019-06-16 14:19:20 +0800

    【Feature】完成图片爬虫部分功能 可以爬去下载xingmeng365网站中的所有的美女图片，虽然网站Home页面被改，网站貌似被遗弃，但是除了Home页面均可正常访问

--- Script/main.py
+++ Script/main.py
@@ -4,42 +4,28 @@
 # 爬取妹子图片
 # http://m.xingmeng365.com
 # http://m.xingmeng365.com/articles.asp?id=2
-# http://m.xingmeng365.com/articles.asp?id=1387
-# http://m.xingmeng365.com/articles.asp?id=1387&mm=2
-# http://m.xingmeng365.com/upload/image/20190531/20190531221061596159.jpg
-# 存储文件地址为 '../photo/'
+# http://m.xingmeng365.com/articles.asp?id=1394
+# http://m.xingmeng365.com/articles.asp?id=1394&mm=2
+# http://img.xingmeng365.com//upload/image/20190610/20190610212229842984.jpg
+# 存储文件地址为 './photo/'
 
 
 import requests
 from bs4 import BeautifulSoup
-import urllib
 import socket
-
-
-# 回调函数
-# @a:已经下载的数据块
-# @b:数据块的大小
-# @c:远程文件的大小
-def cbk(a, b, c):
-    per = 100.0 * a * b / c
-    if per > 100:
-        per = 100
-    print('%.2f%%' % per)
 
 
 if __name__ == '__main__':
     # photo的主地址
-    photo_url = "http://m.xingmeng365.com/upload/"
-    # 保存图片，XXXXX_index.jpg
-    file = '../photo/'
-    # page_number = 1387
-    page_number = 3
-    index_number = 3
-    while page_number > 0:
-        # while index_number < 70:
-        while index_number > 0:
+    photo_url = "http://m.xingmeng365.com/articles.asp?id="
+    # page_number = 1394
+    page_number = 1394
+    index_number = 0
+    photoName = ""
+    while page_number > 1393:
+        while index_number < 70:
             try:
-                # 定义爬虫目标网站地址
+                # 定义爬虫目标网站地址，第一张图和第二张图及其之后URL地址格式不一
                 if index_number == 0:
                     url = 'http://m.xingmeng365.com/articles.asp?id=' + str(page_number)
                 else:
@@ -47,24 +33,34 @@ if __name__ == '__main__':
                 # 定义浏览器标头
                 headers = {'user-agent': 'my-app/0.0.1'}
                 r = requests.get(url, headers=headers)
+                # 解决中文乱码问题
+                r.encoding = 'gb18030'
                 # 获得目标页面返回信息
-                print(r.status_code)
-                print(url)
-                while r.status_code == 200:
+                # print(r.status_code)
+                # print(url)
+                if r.status_code == 500:
+                    break
+                if r.status_code == 200:
                     # 返回的信息放入soup中
                     # 获取页面全部标签信息
                     soup = BeautifulSoup(r.text, 'html.parser')
-                    print(soup.prettify())
+                    # print(soup.prettify())
                     # 测试显示的是否是页面的标签
-                    # for link in soup.find_all(img="div-num"):
+                    for link in soup.find_all(name='h1'):
+                        photoName = str(link).replace("<h1>", "").replace("</h1>", "")
+                    for link in soup.find_all(name='img'):
                         # 输出图片地址
-                        # print(link.get('data-src'))
-                        # 设置超时
-                        # socket.setdefaulttimeout(5.0)
-                        # urllib.request.urlretrieve(link.get('data-src'),
-                        #                            file + '/' + str(page_number) + '_' + str(index_number) + '.jpg', cbk)
-                        # index_number = index_number - 1
-                index_number = index_number - 1
+                        if str(link.get('src'))[:4] == 'http':
+                            print(str(link.get('src')))
+                            imageUrl = str(link.get('src'))
+                            # 设置超时
+                            socket.setdefaulttimeout(5.0)
+                            # 下载图片到photo文件夹中
+                            response = requests.get(imageUrl)
+                            with open('./photo/' + photoName + str(page_number) + '_' + str(index_number) + '.jpg', 'wb') as f:
+                                f.write(response.content)
+                index_number = index_number + 1
             except Exception as e:
-                index_number = index_number - 1
+                index_number = index_number + 1
+                print(e)
         page_number = page_number - 1
